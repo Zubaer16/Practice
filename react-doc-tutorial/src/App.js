94015c9e679e6e6d5dea92d0d6471638ff44e3ab
Zubaer16 <zubaer.ahmed7690@gmail.com>
2025-04-25 01:59:44 +0600

    adding interactivity

--- react-doc-tutorial/src/App.js
+++ react-doc-tutorial/src/App.js
@@ -1,20 +1,54 @@
-const poem = {
-  lines: [
-    'I write, erase, rewrite',
-    'Erase again, and then',
-    'A poppy blooms.',
-  ],
+import { useState } from 'react'
+
+const initialList = [
+  { id: 0, title: 'Big Bellies', seen: false },
+  { id: 1, title: 'Lunar Landscape', seen: false },
+  { id: 2, title: 'Terracotta Army', seen: true },
+]
+
+export default function BucketList() {
+  const [list, setList] = useState(initialList)
+
+  function handleToggle(artworkId, nextSeen) {
+    setList(
+      list.map((artwork) => {
+        if (artwork.id === artworkId) {
+          console.log('milse')
+          return { ...artwork, seen: nextSeen }
+        } else {
+          console.log('milenai')
+          return artwork
+        }
+      })
+    )
+  }
+
+  return (
+    <>
+      <h1>Art Bucket List</h1>
+      <h2>My list of art to see:</h2>
+      <ItemList artworks={list} onToggle={handleToggle} />
+    </>
+  )
 }
 
-export default function Poem() {
-  let output = []
-
-  // Fill the output array
-  poem.lines.forEach((line, i) => {
-    output.push(<hr key={i + '-separator'}></hr>)
-    output.push(<p key={i + '-text'}>{line}</p>)
-    //Remove the first <hr />
-  })
-  output.shift()
-  return <article>{output}</article>
+function ItemList({ artworks, onToggle }) {
+  return (
+    <ul>
+      {artworks.map((artwork) => (
+        <li key={artwork.id}>
+          <label>
+            <input
+              type="checkbox"
+              checked={artwork.seen}
+              onChange={(e) => {
+                onToggle(artwork.id, e.target.checked)
+              }}
+            />
+            {artwork.title}
+          </label>
+        </li>
+      ))}
+    </ul>
+  )
 }
